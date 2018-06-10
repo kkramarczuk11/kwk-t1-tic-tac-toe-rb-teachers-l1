76dd1537199985866b908956ffeee4c96cf7c9ca
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-10 14:25:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,100 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+value = "X"
+def move(board, index, token)
+board[index] = token
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) then return false
+  elsif index.between?(0, 8) then return true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+
+  index = input_to_index(input)
+
+if valid_move?(board, index)
+move(board, index, token = "X")
+display_board(board)
+else
+turn(board)
+until turn(board) == valid_move
+end
+end
+end
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [1,4,7],
+  [0,3,6],
+  [2,5,8]
+]
+def won?(board)
+WIN_COMBINATIONS.detect do |position|
+  if board[position[0]] == "X" && board[position[1]] == "X" && board[position[2]] == "X"
+    position
+  elsif board[position[0]] == "O" && board[position[1]] == "O" && board[position[2]] == "O"
+    position
+  else
+    false
+  end
+end
+end
+
+def full?(board)
+  board.all? do |cell|
+  cell == "X" || cell == "O"
+end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  elsif !won?(board) && !full?(board)
+    false
+  else won?(board)
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+ if solution = won?(board)
+  board[solution.first]
+ else
+  nil
+ end
+end
