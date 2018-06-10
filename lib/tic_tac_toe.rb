d32a9cb58eee65165babf1fce9758db46aaaba01
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-10 14:13:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,19 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def valid_move?(board, index)
+  if position_taken?(board, index) then return false
+  elsif index.between?(0, 8) then return true
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O" then return true
+elsif board[index] == " " || board[index] == nil then return false
+end
+end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
